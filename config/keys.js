73fd8bbc9ab71b81dc74e3f751e7dc6769a24948
dittmanns <dittmanns@ergovia.de>
2019-11-04 12:49:29 +0100

    Edit gitignore and config

--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,3 @@
-mongodb+srv://shopping-app:<password>@cluster0-pivnw.mongodb.net/test?retryWrites=true&w=majority
+module.exports = {
+    mongoURI: 'mongodb+srv://shopping-app:<password>@cluster0-pivnw.mongodb.net/test?retryWrites=true&w=majority'
+}

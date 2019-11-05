607177427817f869358038cd814a2f6e91ebb445
dittmanns <dittmanns@ergovia.de>
2019-11-05 09:10:23 +0100

    Complete Backend

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,4 +13,23 @@ router.get('/', (req, res) => {
         .then(items => res.json(items))
 });
 
+// @route   POST api/items
+// @desc    Creat a Item
+// @access  Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+    
+    newItem.save().then(item => res.json(item));
+});
+
+// @route   Delete api/items/:id
+// @desc    Delete a Item
+// @access  Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({ success: false}));
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose
     .catch(err => console.log(err));
 
 // Use Routes
-app.use('api/items', items);
+app.use('/api/items', items);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1337;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));

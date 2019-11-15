4b52279fe67c017e79ed62bed7f1cc42e6f17613
dittmanns <dittmanns@ergovia.de>
2019-11-15 11:57:05 +0100

    Add Login Part

--- client/src/components/auth/LoginModal.js
+++ client/src/components/auth/LoginModal.js
@@ -13,14 +13,13 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { register } from '../../actions/authActions';
+import { login } from '../../actions/authActions';
 import { clearErrors } from '../../actions/errorActions';
 
 
-class RegisterModal extends Component {
+class LoginModal extends Component {
     state = {
         modal: false,
-        name: '',
         email: '',
         password: '',
         msg: null
@@ -29,15 +28,15 @@ class RegisterModal extends Component {
     static propTypes = {
         isAuthenticated: PropTypes.bool,
         error: PropTypes.object.isRequired,
-        register: PropTypes.func.isRequired,
+        login: PropTypes.func.isRequired,
         clearErrors: PropTypes.func.isRequired
     }
 
     componentDidUpdate(prevProps) {
         const { error, isAuthenticated } = this.props;
         if(error !== prevProps.error) {
-            // Check for register error
-            if(error.id === 'REGISTER_FAIL') {
+            // Check for login error
+            if(error.id === 'LOGIN_FAIL') {
                 this.setState({ msg: error.msg.msg });
             } else {
                 this.setState({ msg: null }); 
@@ -66,48 +65,34 @@ class RegisterModal extends Component {
     onSubmit = e => {
         e.preventDefault();
         
-        const { name, email, password } = this.state;
-        
-        // Create user object
-        const newUser = {
-            name,
+        const { email, password } = this.state;
+
+        const user = {
             email,
             password
-        };
-
-        // Attempt to register
-        this.props.register(newUser);
-
+        }
+        // Attempt to login
+        this.props.login(user);
     };
 
     render() {
         return(
             <div>
                 <NavLink onClick={this.toggle} href="#">
-                    Register
+                    Login
                 </NavLink>
 
                 <Modal
                     isOpen={this.state.modal}
                     toggle={this.toggle}
                 >
-                    <ModalHeader toggle={this.toggle}>Register</ModalHeader>
+                    <ModalHeader toggle={this.toggle}>Login</ModalHeader>
                     <ModalBody>
                         {this.state.msg ? (
                             <Alert color='danger'>{this.state.msg}</Alert>
                         ) : null }
                         <Form onSubmit={this.onSubmit}>
                             <FormGroup>
-                                <Label for='name'>Name</Label>
-                                <Input 
-                                    type='text' 
-                                    name='name' 
-                                    id='name' 
-                                    placeholder='Name'
-                                    className='mb-3' 
-                                    onChange={this.onChange}
-                                />
-
                                 <Label for='email'>Email</Label>
                                 <Input 
                                     type='email' 
@@ -131,7 +116,7 @@ class RegisterModal extends Component {
                                     color='dark'
                                     style={{marginTop: '2rem'}}
                                     block
-                                >Register</Button>
+                                >Login</Button>
 
                             </FormGroup>
                         </Form>
@@ -149,5 +134,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    { register, clearErrors }
-)(RegisterModal);
+    { login, clearErrors }
+)(LoginModal);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import itemReducer from './itemReducer';
-import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
     item: itemReducer,
-    auth: authReducer,
     error: errorReducer,
+    auth: authReducer,
 });

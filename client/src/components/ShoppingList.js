6e7502b39858080e74a206dfb677d5e9bb1c85ba
dittmanns <dittmanns@ergovia.de>
2019-11-18 08:37:14 +0100

    Add single Login

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -6,10 +6,15 @@ import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
- 
+    static propTypes = {
+        getItems: PropTypes.func.isRequired,
+        item: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    };
+
     componentDidMount() {
         this.props.getItems();
-    }
+    };
 
     onDeleteClick = id => {
         this.props.deleteItem(id);
@@ -25,12 +30,13 @@ class ShoppingList extends Component {
                         {items.map(({ _id, name }) => (
                             <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
+                                { this.props.isAuthenticated ? 
                                 <Button
                                     className="remove-btn"
                                     color="danger"
                                     size="sm"
                                     onClick={this.onDeleteClick.bind(this, _id)}
-                                >&times;</Button>
+                                >&times;</Button> : null}
                                 {name}
                                 </ListGroupItem>
                             </CSSTransition>
@@ -42,13 +48,10 @@ class ShoppingList extends Component {
     }
 }
 
-ShoppingList.propTypes = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired
-}
 
 const mapStateToProps = (state) => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(

011a8e50421d4d55c040f583a67faa51ad202490
dittmanns <dittmanns@ergovia.de>
2019-12-09 14:58:01 +0100

    preparing to deploy

--- server.js
+++ server.js
@@ -27,15 +27,12 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
 
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 48340;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));

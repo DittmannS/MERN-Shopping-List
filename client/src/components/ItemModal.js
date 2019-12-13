c8dcd8fbe74c47d94e32501b9b5fa9f7c0b5adf2
dittmanns <dittmanns@ergovia.de>
2019-12-13 08:33:08 +0100

    add autofocus to dialog, change langage

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -65,23 +65,24 @@ class ItemModal extends Component {
                     isOpen={this.state.modal}
                     toggle={this.toggle}
                 >
-                    <ModalHeader toggle={this.toggle}>Add to Shopping List</ModalHeader>
+                    <ModalHeader toggle={this.toggle}>Artikel zur Einkaufliste hinzufügen</ModalHeader>
                     <ModalBody>
                         <Form onSubmit={this.onSubmit}>
                             <FormGroup>
-                                <Label for="item">Item</Label>
+                                <Label for="item">Artikel</Label>
                                 <Input 
                                     type="text" 
                                     name="name" 
                                     id="item" 
-                                    placeholder="Add shopping Item" 
+                                    placeholder="neuer Artikel" 
                                     onChange={this.onChange}
+                                    autofocus="true"
                                 />
                                 <Button
                                     color="dark"
                                     style={{marginTop: '2rem'}}
                                     block
-                                >Add Item</Button>
+                                >Artikel eintragen</Button>
 
                             </FormGroup>
                         </Form>

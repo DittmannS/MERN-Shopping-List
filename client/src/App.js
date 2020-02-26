f400b9ff831e4ce15f468120a74da676412bef48
dittmanns <dittmanns@ergovia.de>
2020-02-26 14:22:33 +0100

    Connect Frontend to Backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,43 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 import ItemState from './context/item/ItemState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
+
+if(localStorage.token) {
+    setAuthToken(localStorage.token);
+}
 
 const App = () => {
     return (
-        <ItemState>
-            <Router>
-                <Fragment>
-                    <Navbar />
-                    <div className="container">
-                        <Switch>
-                            <Route exact path='/' component={Home} />
-                            <Route exact path='/about' component={About} />
-                        </Switch>    
-                    </div>    
-                </Fragment>
-            </Router>
-        </ItemState>
+        <AuthState>
+            <ItemState>
+                <AlertState>
+                    <Router>
+                        <Fragment>
+                            <Navbar />
+                            <div className="container">
+                                <Alerts />
+                                <Switch>
+                                    <Route exact path='/' component={Home} />
+                                    <Route exact path='/about' component={About} />
+                                    <Route exact path='/register' component={Register} />
+                                    <Route exact path='/login' component={Login} />
+                                </Switch>    
+                            </div>    
+                        </Fragment>
+                    </Router>
+                </AlertState>
+            </ItemState>
+        </AuthState>
     );
 }
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Items from '../items/Items';
 import ItemForm from '../items/ItemForm';
 import ItemFilter from '../items/ItemFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        authContext.loadUser();
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <div className='grid-2'>
             <div> <ItemForm /> </div>
